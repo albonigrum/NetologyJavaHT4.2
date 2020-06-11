a6437a58dadbb9a1113e2aa5718f3bfcc3835e7b
Ivan Grebenschikov <albonigrum@gmail.com>
2020-06-11 16:04:18 +0300

    Add BMIService.BMIResult and BMIService.calculateStatus Improve formula in BMIService.calculate Renaming variables

--- src/BMIService.java
+++ src/BMIService.java
@@ -1,5 +1,50 @@
 public class BMIService {
-    float calculate(float weight, float height) {
-        return weight / height * height;
+    public enum BMIResult {
+        SEVERE_THINNESS("SEVERE_THINNESS"),
+        MODERATE_THINNESS("MODERATE_THINNESS"),
+        MILD_THINNESS("MILD_THINNESS"),
+        NORMAL("NORMAL"),
+        OVERWEIGHT("OVERWEIGHT"),
+        OBESE_CLASS_1("OBESE_CLASS_1"),
+        OBESE_CLASS_2("OBESE_CLASS_2"),
+        OBESE_CLASS_3("OBESE_CLASS_3");
+
+        final private String title;
+
+        BMIResult(String title) {
+            this.title = title;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        @Override
+        public String toString() {
+            return title;
+        }
+    }
+    float calculate(float weightInKilograms, float heightInMetres) {
+        return weightInKilograms / heightInMetres / heightInMetres;
+    }
+
+    BMIResult calculateStatus(float weightInKilograms, float heightInMetres) {
+        float bmi = calculate(weightInKilograms, heightInMetres);
+        if (bmi <= 16)
+            return BMIResult.SEVERE_THINNESS;
+        else if (bmi <= 17)
+            return BMIResult.MODERATE_THINNESS;
+        else if (bmi <= 18.5)
+            return BMIResult.MILD_THINNESS;
+        else if (bmi <= 25)
+            return BMIResult.NORMAL;
+        else if (bmi <= 30)
+            return BMIResult.OVERWEIGHT;
+        else if (bmi <= 35)
+            return BMIResult.OBESE_CLASS_1;
+        else if (bmi <= 40)
+            return BMIResult.OBESE_CLASS_2;
+        else
+            return BMIResult.OBESE_CLASS_3;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         BMIService bmiService = new BMIService();
-        int weight, height;
-        weight = 90;
-        height = 190;
-        System.out.println(bmiService.calculate(weight, height));
+        float weightInKilograms, heightInMetres;
+        weightInKilograms = 85.3F;
+        heightInMetres = 1.88F;
+        System.out.println(bmiService.calculate(weightInKilograms, heightInMetres));
+        System.out.println(bmiService.calculateStatus(weightInKilograms, heightInMetres));
     }
 }
